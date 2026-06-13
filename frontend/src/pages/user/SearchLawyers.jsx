384aa1137bf381fa5fe3f2f1f804779f5e0a1b13
Thirushi Parindya Peiris <thirushiparindya@gmail.com>
2026-06-13 10:04:31 +0530

    Add initial imports to SearchLawyers component

---
+++ frontend/src/pages/user/SearchLawyers.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { Search, Star, MapPin, Briefcase, Filter, ArrowRight, CheckCircle } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
