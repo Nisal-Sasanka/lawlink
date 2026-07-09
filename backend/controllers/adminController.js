772b78675b2212fb0b9b29ddde5160f682fd0ade
Nisal-Sasanka <sasankajayalal.02@gmail.com>
2026-07-09 15:34:16 +0530

    Updateadmin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,6 @@
 import db from "../config/db.js";
 
-export const getAllUserss = async (req, res) => {
+export const getAllUsers = async (req, res) => {
   const result = await db.query("SELECT * FROM users");
   res.json(result.rows);
 };
