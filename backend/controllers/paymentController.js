a98010de01a67db6960de2d950af68236abd0b12
Nisal-Sasanka <sasankajayalal.02@gmail.com>
2026-07-09 18:39:16 +0530

    Modify payment

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,6 +1,6 @@
 import db from "../config/db.js";
 
-export const getAllPayments = async (req, res) => {
+export const getAllPaymentss = async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM payments");
     res.json(result.rows);
