447e6305809cd2cbce85f376e6d16a9d02ab5e60
Thirushi Parindya Peiris <thirushiparindya@gmail.com>
2026-08-01 12:54:06 +0530

    modified

--- backend/routes/documentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import upload from "../middleware/uploadMiddleware.js";
-import { uploadComplaintDocument } from "../controllers/documentController.js";
-
-const router = express.Router();
-
-router.post("/upload", upload.single("document"), uploadComplaintDocument);
-
-export default router;
--- backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllReviews, createReview } from "../controllers/reviewController.js";
-
-const router = express.Router();
-
-router.get("/", getAllReviews);
-router.post("/", createReview);
-
-export default router;
