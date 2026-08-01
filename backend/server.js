6c6b662561031abc40787814310195673b201f14
Jayalal H.A.N.S. <sasankajayalal.02@gmail.com>
2026-08-01 12:39:16 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,1 @@
-const express = require("express");
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("LawLink Backend Running");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
