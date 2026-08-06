412abefcbaffde887ea7580cb9d2e4b92d955daf
Pavithar <pavitharmangaleswaran@gmail.com>
2026-08-06 07:13:25 +0530

    Fix  SQL syntax for Package table

--- backend/prisma/migrations/20260706181336_init_mysql/migration.sql
+++ backend/prisma/migrations/20260706181336_init_mysql/migration.sql
@@ -41,7 +41,7 @@ CREATE TABLE `LawyerProfile` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- CreateTable
+--- CreateTable
 CREATE TABLE `Package` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
